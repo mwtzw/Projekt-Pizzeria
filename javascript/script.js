51000e95d61119a280fde9d6402fb9eb6a3a3510
Matitek24 <k.skrzypek1234@gmail.com>
2023-03-26 16:42:41 +0200

    dzialajacy js skladniki

--- javascript/script.js
+++ javascript/script.js
@@ -8,57 +8,92 @@ let nr = document.getElementsByName("nr")
 let price = document.querySelector("#price")
 let wynik = 20
 let product_price = 0
+let product_price1 = 0
+let product_price2 = 0
+let product_price3 = 0
+let product_price4 = 0
 setTimeout(() => {
 
-        for(let i=0; i<4;i++){
+        for(let i=0; i<5;i++){
             plus[i].addEventListener("click",()=>{
                 if(i==0){
                     product_price = parseInt(nr[i].value) * 2
-                    wynik += product_price
                 }
                 if(i==1){
-                    product_price = parseInt(nr[i].value) * 5
-                    wynik += product_price
+                    product_price1 = parseInt(nr[i].value) * 5
                 }
                 if(i==2){
-                    product_price = parseInt(nr[i].value) * 1
-                    wynik += product_price
+                    product_price2 = parseInt(nr[i].value) * 1
                 }
                 if(i==3){
-                    product_price = parseInt(nr[i].value) * 4
-                    wynik += product_price
+                    product_price3 = parseInt(nr[i].value) * 4
                 }
                 if(i==4){
-                    product_price = parseInt(nr[i].value) * 3
-                    wynik += product_price
+                    product_price4 = parseInt(nr[i].value) * 3
                 }
-                price.innerHTML = product_price +"zł"  
+                setTimeout(()=>{
+                    price.classList.add("text_green")
+                    price.classList.remove("text_white")
+                    price.innerHTML = 20 + product_price + product_price1 + product_price2 + product_price3 + product_price4 +"zł" 
+                    setTimeout(()=>{
+                        price.classList.add("text_white")
+                        price.classList.remove("text_green")
+                
+                    },"500")
+                }, "100")
+        
             })
             minus[i].addEventListener("click",()=>{
                 if(i==0){
-                    wynik -= 2
-                    price.innerHTML = wynik +"zł"  
+                    if(parseInt(nr[i].value) <= 0){
+                        product_price = 0 
+                    }
+                    else{
+                        product_price -= 2   
+                    }
                 }
                 if(i==1){
-                    product_price = parseInt(nr[i].value) * 5
-                    wynik -= 5
-                    price.innerHTML = wynik +"zł"  
+                    if(parseInt(nr[i].value ) <= 0){
+                        product_price1 = 0 
+                    }
+                    else{
+                        product_price1 -= 5  
+                    }
                 }
                 if(i==2){
-                    product_price = parseInt(nr[i].value) * 1
-                    wynik -= 1
-                    price.innerHTML = wynik +"zł"  
+                    if(parseInt(nr[i].value) <= 0){
+                        product_price2 = 0 
+                    }
+                    else{
+                        product_price2 -= 1  
+                    }
                 }
                 if(i==3){
-                    product_price = parseInt(nr[i].value) * 4
-                    wynik -= 4
-                    price.innerHTML = wynik +"zł"  
+                    if(parseInt(nr[i].value) <= 0){
+                        product_price3 = 0 
+                    }
+                    else{
+                        product_price3 -= 4   
+                    }
                 }
                 if(i==4){
-                    product_price = parseInt(nr[i].value) * 3
-                    wynik -= 3
-                    price.innerHTML = wynik +"zł"  
+                    if(parseInt(nr[i].value) <= 0){
+                        product_price4 = 0 
+                    }
+                    else{
+                        product_price4 -= 3   
+                    }
                 }
+                setTimeout(()=>{
+                    price.classList.add("text_red")
+                    price.classList.remove("text_white")
+                    price.innerHTML = 20 + product_price + product_price1 + product_price2 + product_price3 + product_price4 +"zł" 
+                    setTimeout(()=>{
+                        price.classList.add("text_white")
+                        price.classList.remove("text_red")
+                
+                    },"500")
+                }, "100")
             }) 
         }
 
