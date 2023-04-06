95345c63aaf70277d12345215b33fd4780b4e50c
Matitek24 <k.skrzypek1234@gmail.com>
2023-04-07 00:41:44 +0200

    REGISTER FRONT

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 $(function(){
     $('input[type="number"]').niceNumber();
 });
+
  // do ogarniecia JS aby cena się zmieniała zgodnei z cennikiem tyle ile produktów się doda to taka cena 
 let plus = document.getElementsByClassName("plus")
 let minus = document.getElementsByClassName("minus")
@@ -97,6 +98,32 @@ setTimeout(() => {
             }) 
         }
 
-    
+        let register = document.querySelector("#res")
+        let login = document.querySelector("#log")
+        let form1 = document.querySelector("#form1")
+        let form2 = document.querySelector("#form2")
+
+        register.addEventListener('click', ()=>{
+            form1.classList.add("d-none")
+            form1.classList.remove("d-block")
+            form2.classList.add("d-block")
+            form2.classList.remove("d-none")
+        })
+        login.addEventListener('click', ()=>{
+            form1.classList.add("d-block")
+            form1.classList.remove("d-none")
+            form2.classList.add("d-none")
+            form2.classList.remove("d-block")
+        })
+        let loginbutton = document.getElementsByClassName("button-style")
+        for(let n = 0; loginbutton.length > n; n++){
+            loginbutton[n].addEventListener('click',()=>{
+                form1.classList.add("d-block")
+                form1.classList.remove("d-none")
+                form2.classList.add("d-none")
+                form2.classList.remove("d-block")
+            }) 
+        }
   }, "500");
+
 
