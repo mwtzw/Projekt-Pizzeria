52acef1e0b187eb4393dc6fcd71d09c838b27034
mwtzw <126615274+mwtzw@users.noreply.github.com>
2023-04-12 17:40:46 +0200

    user

--- php/config.php
+++ php/config.php
@@ -4,4 +4,6 @@ define("MYSQL_USER", 'root');
 define("MYSQL_PASSWORD", '');
 define("MYSQL_HOST", 'localhost');
 define("MYSQL_DATABASE", 'pizzeria');
-$_SESSION['userSignedIn'] = false;
+if(!isset($_SESSION['isLoggedIn'])) {
+    $_SESSION['isLoggedIn'] = false;
+}
