7080e3546060be54600476446c95c7c01e62b67a
mwtzw <126615274+mwtzw@users.noreply.github.com>
2023-04-15 22:44:21 +0200

    Nowa wersja bazy danych

--- pizzeria_v5.sql
+++ pizzeria_v5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 13, 2023 at 10:19 PM
+-- Generation Time: Apr 15, 2023 at 10:34 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,10 +42,9 @@ CREATE TABLE `baskets` (
 INSERT INTO `baskets` (`id`, `userId`, `basketData`) VALUES
 (1, 1, '{\"products\":[],\"customProducts\":[]}'),
 (2, 2, '{\"products\":[],\"customProducts\":[]}'),
-(3, 3, '{\"products\": [{\"id\": 1, \"size\": 60, \"count\": 1}, {\"id\": 4, \"size\": 30, \"count\": 1}], \"customProducts\": []}'),
-(4, 4, '{\"products\": [], \"customProducts\": []}'),
-(5, 8, '{\"products\":[{\"id\":3,\"count\":1,\"size\":45}],\"customProducts\":[]}'),
-(6, 11, '{\"products\":[{\"id\":1,\"count\":1,\"size\":45}],\"customProducts\":[]}');
+(3, 3, '{\"products\":[],\"customProducts\":[]}'),
+(4, 4, '{\"products\":[],\"customProducts\":[]}'),
+(7, 12, '{\"products\":[],\"customProducts\":[]}');
 
 -- --------------------------------------------------------
 
@@ -103,7 +102,9 @@ INSERT INTO `orders` (`id`, `userId`, `orderData`, `address`, `paymentMethod`, `
 (19, 1, '{\"products\":[{\"id\":2,\"count\":1,\"size\":45},{\"id\":1,\"count\":1,\"size\":45}],\"customProducts\":[]}', 'Nieznana 10', 1, 71.00, '2023-04-13 20:40:21', '12345678'),
 (20, 1, '{\"products\":[{\"id\":4,\"count\":1,\"size\":60},{\"id\":2,\"count\":1,\"size\":30},{\"id\":1,\"count\":1,\"size\":45}],\"customProducts\":[]}', 'Wyszków, Serwerowa 11', 2, 114.60, '2023-04-13 21:36:23', '12345678'),
 (21, 1, '{\"products\":[{\"id\":3,\"count\":1,\"size\":45}],\"customProducts\":[]}', 'Wyszków, Serwerowa 11', 1, 52.00, '2023-04-13 21:37:58', '12345678'),
-(22, 1, '{\"products\":[{\"id\":1,\"count\":3,\"size\":45}],\"customProducts\":[]}', 'Wyszków, Serwerowa 11', 3, 111.00, '2023-04-13 21:38:31', '12345678');
+(22, 1, '{\"products\":[{\"id\":1,\"count\":3,\"size\":45}],\"customProducts\":[]}', 'Wyszków, Serwerowa 11', 3, 111.00, '2023-04-13 21:38:31', '12345678'),
+(23, 1, '{\"products\":[{\"id\":4,\"count\":5,\"size\":45}],\"customProducts\":[]}', 'Wyszków, Serwerowa 11', 1, 210.00, '2023-04-14 22:41:50', '12345678'),
+(24, 1, '{\"products\":[],\"customProducts\":[{\"count\":1,\"ingredients\":[{\"id\":1,\"count\":5},{\"id\":2,\"count\":5},{\"id\":3,\"count\":5},{\"id\":4,\"count\":5},{\"id\":5,\"count\":5}]},{\"count\":1,\"ingredients\":[{\"id\":2,\"count\":5},{\"id\":4,\"count\":5}]}]}', 'Nieznana 10', 1, 160.00, '2023-04-15 00:37:48', '12345678');
 
 -- --------------------------------------------------------
 
@@ -136,10 +137,8 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `price` decimal(10,0) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
   `shortDescription` varchar(50) NOT NULL,
-  `ingredients` varchar(150) NOT NULL,
-  `sold` int(11) NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
@@ -147,11 +146,38 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `shortDescription`, `ingredients`, `sold`, `image`) VALUES
-(1, 'Peperoni', 'Pizza z peperoni to bardzo smaczny wybór na ciepłe wieczory', 37, 'Pizza z peperoni', 'Sos Pomidorowy, Ser Mozzarella, Pepperoni', 12, 'img/products/peperoni.png'),
-(2, 'Hawajska', 'Pizza z ananasem to bardzo smaczny wybór na ciepłe wieczory', 34, 'Pizza z ananasem', 'Sos Pomidorowy, Ser Mozzarella, Szynka, Ananas', 2, 'img/products/hawai.png'),
-(3, 'Mięsna PRO', 'Pizza z mięsem to bardzo smaczny wybór na zimne wieczory', 52, 'Pizza z mięsem', 'Sos Pomidorowy, Ser Mozzarella, Pepperoni, Wolowina, Wieprzowina', 20, 'img/products/meat.png'),
-(4, 'Mięsna', 'Pizza z mięsem to bardzo smaczny wybór na zimne wieczory', 42, 'Pizza z mięsem', 'Sos Pomidorowy, Ser Mozzarella, Pepperoni, Wolowina, Wieprzowina', 21, 'img/products/meat.png');
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `shortDescription`, `image`) VALUES
+(1, 'Peperoni', 'Pizza z peperoni to bardzo smaczny wybór na ciepłe wieczory', 37.00, 'Pizza z peperoni', 'img/products/peperoni.png'),
+(2, 'Hawajska', 'Pizza z ananasem to bardzo smaczny wybór na ciepłe wieczory', 34.00, 'Pizza z ananasem', 'img/products/hawai.png'),
+(3, 'Mięsna PRO', 'Pizza z mięsem to bardzo smaczny wybór na zimne wieczory', 52.00, 'Pizza z mięsem', 'img/products/meat.png'),
+(4, 'Mięsna', 'Pizza z mięsem to bardzo smaczny wybór na zimne wieczory', 42.00, 'Pizza z mięsem', 'img/products/meat.png'),
+(6, 'Margarita', 'Pizza z serem to bardzo smaczny wybór na zimne wieczory', 22.00, 'Pizza z serem', 'img/products/margharita.png'),
+(7, 'Wiejska', 'Pizza z oliwkami to bardzo smaczny wybór na zimne wieczory', 45.50, 'Pizza wiejska', 'img/products/wiejska.png'),
+(8, 'Capriciosa', 'Pizza z grzybami to bardzo smaczny wybór na ciepłe wieczory', 27.00, 'Pizza z grzybem', 'img/products/capriciosa.png'),
+(9, 'Bazyliana', 'Pizza z bazylią to bardzo smaczny wybór na ciepłe wieczory', 52.00, 'Pizza z bazylią', 'img/products/bazyliana.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `review` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `userId`, `review`) VALUES
+(1, 1, 'Pizzeria z cudownym smakiem, obsługą i atmosferą! Wybór pizz imponujący, ciasto cienkie i chrupiące. Świeże składniki. '),
+(2, 2, 'Pizzeria z doskonałymi pizzami i przyjazną obsługą! Świeże składniki, cienkie ciasto, niepowtarzalny smak. Polecam!'),
+(3, 3, 'Niesamowite pizze, miła obsługa, świeże składniki. Polecam to miejsce!\r\n'),
+(4, 4, 'Pyszne pizze, świetna atmosfera, profesjonalna obsługa. Idealne miejsce na spotkania z rodziną i przyjaciółmi. Gorąco polecam!'),
+(5, 12, 'Pizza była przeciętna, obsługa nieco nieuprzejma, długi czas oczekiwania na zamówienie. Atmosfera nieco głośna. Nie do końca spełniło oczekiwania.');
 
 -- --------------------------------------------------------
 
@@ -173,7 +199,9 @@ CREATE TABLE `specialOffers` (
 --
 
 INSERT INTO `specialOffers` (`id`, `userId`, `promoCode`, `discount`, `image`, `isUsed`) VALUES
-(1, 1, 'D234XS3', 0.2, 'img/specialOffers/promocja.png', 0);
+(1, 1, 'D234XS3', 0.2, 'img/specialOffers/promocja.png', 0),
+(2, 1, 'A2454XS3', 0.3, 'img/specialOffers/promocja2.png', 0),
+(3, 1, 'G23BA34L', 0.1, 'img/specialOffers/promocja3.png', 0);
 
 -- --------------------------------------------------------
 
@@ -198,10 +226,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `email`, `phone`, `password`, `name`, `surname`, `image`) VALUES
 (1, 'ps@example.net', '507337591', '1a1dc91c907325c69271ddf0c944bc72', 'Patryk', 'Sawicki', 'img/users/default.png'),
 (2, 'lw@example.net', '368522188', '1a1dc91c907325c69271ddf0c944bc72', 'Łukasz', 'Włodarczyk', 'img/users/user1.png'),
-(3, 'bw@example.net', '368522188', '1a1dc91c907325c69271ddf0c944bc72', 'Blanka', 'Wójcik', 'img/users/user2.png'),
-(4, 'as@example.net', '132140954', '1a1dc91c907325c69271ddf0c944bc72', 'Agata', 'Szulc', 'img/users/user3.png'),
-(8, 'test@mail.com', '12345678', 'dcebbce69bab3b12ae6c9a7d6cfbbd9f', 'Test', 'Te', 'img/users/default.png'),
-(11, 't@t.t', '023456789', '1a1dc91c907325c69271ddf0c944bc72', 'Test2', 'Test2', 'img/users/default.png');
+(3, 'bw@example.net', '368522188', '1a1dc91c907325c69271ddf0c944bc72', 'Blanka', 'Wójcik', 'img/users/user3.png'),
+(4, 'as@example.net', '132140954', '1a1dc91c907325c69271ddf0c944bc72', 'Andrzej', 'Szulc', 'img/users/user2.png'),
+(12, 'an@example.com', '555123456', '1a1dc91c907325c69271ddf0c944bc72', 'Anna', 'Nowak', 'img/users/user4.png');
 
 --
 -- Indexes for dumped tables
@@ -241,6 +268,13 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
 -- Indexes for table `specialOffers`
 --
 ALTER TABLE `specialOffers`
@@ -261,19 +295,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `baskets`
 --
 ALTER TABLE `baskets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `paymentMethods`
@@ -285,19 +319,25 @@ ALTER TABLE `paymentMethods`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `specialOffers`
 --
 ALTER TABLE `specialOffers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -307,7 +347,7 @@ ALTER TABLE `users`
 -- Constraints for table `baskets`
 --
 ALTER TABLE `baskets`
-  ADD CONSTRAINT `baskets_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `baskets_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `orders`
@@ -315,6 +355,12 @@ ALTER TABLE `baskets`
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`paymentMethod`) REFERENCES `paymentMethods` (`id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `specialOffers`
